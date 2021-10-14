dbd64ee5ae8271db061fb777f6c5ab5147cc4897
johns_zp <johns_zp@qq.com>
2021-10-14 15:54:16 +0800

    Create rotate_matrix.py

---
+++ Leetcode/rotate_matrix/rotate_matrix.py
@@ -0,0 +1,41 @@
+from typing import List
+class Solution:
+    angle = 90
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        if Solution.angle == 90:  # 顺时针旋转90 是先转置 再做左右镜像
+            self.rightleftImage(self.Transpose(matrix))
+        elif Solution.angle == 180:  # 顺时针旋转180 则是上下， 左右各做镜像
+            self.updownImage(self.rightleftImage(matrix))
+        elif Solution.angle == 270:  # 顺时针270度 则是先转置， 后做上下镜像
+            self.updownImage(self.Transpose(matrix))
+        else:
+            matrix
+
+    def Transpose(self, matrix: List[List[int]]) -> list:
+        """
+        转置
+        """
+        for i in range(len(matrix)):
+            for x in range(i, len(matrix)):
+                matrix[i][x], matrix[x][i] = matrix[x][i], matrix[i][x]    
+        return matrix
+        
+    def updownImage(self, matrix: List[List[int]]) -> list:
+        """
+        上下镜像
+        """
+        for x,y in zip(range(int(len(matrix) / 2)), range(len(matrix)-1, int(len(matrix) / 2 - 0.5), -1)):
+            matrix[x], matrix[y] = matrix[y], matrix[x]
+        return matrix
+
+    def rightleftImage(self, matrix: List[List[int]]) -> list:
+        """
+        左右镜像
+        """
+        for i in range(len(matrix)):
+            for x,y in zip(range(int(len(matrix) / 2)), range(len(matrix)-1, int(len(matrix) / 2 - 0.5), -1)):
+                matrix[i][x], matrix[i][y] = matrix[i][y], matrix[i][x]    
+        return matrix
