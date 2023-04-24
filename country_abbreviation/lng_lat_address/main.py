a23ab575529e36301051032b28c947810b947d99
peng.zhang <peng.zhang@tenclass.com>
2023-04-24 16:05:46 +0800

    添加demo

--- country_abbreviation/lng_lat_address/main.py
+++ country_abbreviation/lng_lat_address/main.py
@@ -12,6 +12,11 @@ def run(lng: float, lat: float, model:str = 'GCJ02') -> [str, None]:
 
 
 if __name__ == "__main__":
+    model_ = {'1': 'GCJ02',
+              '2': 'BD09'}.get(input('1: "GCJ02", 2: "BD09": '), None)
+    print(model_)
     lng = float(input("lng: "))
     lat = float(input("lat: "))
-    print(run(lng=lng, lat=lat))
+    print(run(lng=lng, 
+              lat=lat, 
+              model=model_ if model_ else 'GCJ02'))
